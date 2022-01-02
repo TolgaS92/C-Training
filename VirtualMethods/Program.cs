a77d121c9e42a98d630d1f1e5787a022d730fe00
TolgaS92 <tolgasecme@icloud.com>
2022-01-02 00:58:06 -0700

    Inheritance / VirtualMethods

---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sqlserver sqlServer = new Sqlserver();
+            sqlServer.Add();
+            MySqlserver mySql = new MySqlserver();
+            mySql.Add();
+
+            Console.ReadLine();
+        }
+    }
+    class Database
+    {
+        public virtual void Add()
+        {
+            Console.WriteLine("Added by Default");
+        }
+        public virtual void Delete()
+        {
+            Console.WriteLine("Deleted by Default");
+        }
+    }
+    class Sqlserver : Database
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Added by SQL Code");
+            //base.Add();
+        }
+    }
+    class MySqlserver : Database
+    {
+
+    }
+}
