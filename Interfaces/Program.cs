1ec4cdd4a9443fc8324a2ae438c28c2c82224864
TolgaS92 <tolgasecme@icloud.com>
2022-01-02 00:28:21 -0700

    Interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //InterfacesIntro();
+            //Demo();
+
+            ICustomerDal[] customerDals = new ICustomerDal[3]
+            {
+                new SqlServerCustomerDal(),
+                new OracleServerCustomerDal(),
+                new MySqlServerCustomerDal()
+            };
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+            Console.ReadLine();
+        }
+
+        private static void Demo()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new OracleServerCustomerDal());
+        }
+
+        private static void InterfacesIntro()
+        {
+            PersonManager manager = new PersonManager();
+            //manager.Add(new Customer { Id = 1, FirstName = "Tolga", LastName = "Secme", Address = "Denver" });
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Tolga",
+                LastName = "Secme",
+                Address = "Denver"
+            };
+            Student student = new Student
+            {
+                Id = 1,
+                FirstName = "Murphy",
+                LastName = "Secme",
+                Department = "Home Office"
+            };
+            manager.Add(customer);
+            manager.Add(student);
+        }
+    }
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+    }
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+
+    }
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
